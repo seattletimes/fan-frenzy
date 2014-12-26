bce6035d283c96c85dacd6bd422424d7f4223dd4
Katrina Barlow <kbarlow@seattletimes.com>
2014-12-26 13:20:10 -0800

    Adds share button

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,10 @@
 //Use CommonJS style via browserify to load other modules
+
+var Share = require("share");
+
+//Share button
+var share = window.share = new Share(".share", {
+  ui: {
+    flyout: "bottom center"
+  }
+});
